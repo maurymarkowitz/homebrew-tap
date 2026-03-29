98662053f82615614657c20f894c1feff55ce908
Maury Markowitz <maury.markowitz@gmail.com>
2026-03-29 11:58:25 -0400

    add instructions for documentation and examples
    Updated the download URL and added installation of library and documentation files.

--- Formula/onec.rb
+++ Formula/onec.rb
@@ -1,13 +1,22 @@
 class Onec < Formula
   desc "OpenNEC antenna simulator"
   homepage "https://github.com/maurymarkowitz/OpenNEC"
-  url "https://api.github.com/repos/maurymarkowitz/OpenNEC/tarball/v1.3.0"
+  url "https://github.com/maurymarkowitz/OpenNEC/archive/refs/tags/v1.3.0.tar.gz"
   sha256 "cc44f03c3254d06724789c1334e14aea378211ea1564678f2060cf0e06c7281c"
   license "MIT"
 
   def install
     system "make"
+
     bin.install "onec"
+    lib.install "libonec.a"
+    man1.install "doc/onec.1"
+
+    # Full documentation tree
+    doc.install Dir["doc/*"]
+
+    # Examples bundled with documentation
+    doc.install "examples"
   end
 
   test do
